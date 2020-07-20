a8ce047a1faba08d887f48b6653acb7c5f7a378e
Kyle Kowal <kkowalnotb@gmailcom>
2020-07-19 21:54:38 -0400

    Creating quiz and high score js elements

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Quiz
+const timerEl = document.querySelector("header span#time");
+const startDivEl = document.querySelector("div.start-div");
+const startButton = document.querySelector("button#start");
+const questionDivEl = document.querySelector("div.question-div");
+const answerButtons = document.querySelector("div.buttons");
+const questionEl = document.querySelector("h1#question");
+const rightWrongEl = document.querySelector("div#right-wrong");
 
+const endDivEl = document.querySelector("div.end-div");
+const finalScoreEl = document.querySelector("h2 span#final-score");
+const scoreForm = document.querySelector("form#score-form");
+const initialsInput = document.querySelector("input#initials");
+
+// High Scores
+const hofLink = document.querySelector("header div#hs");
+const hallOfFame = document.querySelector("div.high-scorers");
+const scoresList = document.querySelector("ol#scores-list");
+const clearHofBtn = document.querySelector("button#clear");
+const goBackHofBtn = document.querySelector("button#go-back");
 
 
